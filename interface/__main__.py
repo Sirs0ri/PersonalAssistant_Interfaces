81768f2e1a807bd7fd1c86db2f246e01543bbd28
Maximilian Ertl <ertl.maxi@gmail.com>
2016-04-11 01:11:44 +0200

    Added logging. Like, a lot
    * Handlers for CLI-output, logging to a file and sending important
    messages to autoremote.
    * A LOT of logger.XY() calls
    Also, a lot of explainig comments

--- interface/__main__.py
+++ interface/__main__.py
@@ -2,6 +2,120 @@
 from the root folder. After importing it, the interface will be
 started start"""
 
-if __name__ == '__main__':
+import logging
+import time
 import simple_cli as the_interface
+import variables_private
+
+
+class AutoRemoteHandler(logging.Handler):
+    """A Handler that sends logging messages to AutoRemote"""
+
+    def emit(self, record):
+        import requests
+        logging.getLogger("requests").setLevel(logging.WARNING)
+        message = self.format(record)
+        # print "--- " + message + " ---"
+        payload = {'key': variables_private.ar_key, 'message': message}
+        requests.post("https://autoremotejoaomgcd.appspot.com/sendmessage",
+                      payload)
+
+
+class ColorStreamHandler(logging.StreamHandler):
+    """A Handler that prints colored messages to the current console."""
+
+    def emit(self, record):
+        """
+        This is the function that actually 'logs' the record. Here, it reads
+        the part of the record containing the levelname (such as "DEBUG" or
+        "ERROR") and adds ANSI-escape-codes around it to change the color.
+        (see: http://ascii-table.com/ansi-escape-sequences.php)
+
+        The String "DEBUG" will be replaced by "\033[96mDEBUG\033[0m" this way,
+        in which "\033[" is the beginning, "m" the end of the escape sequence
+        and 96 the actual Formatter, in this case the code for Cyan foreground
+        text.
+
+        If the levelname specified in the Formatter should have a specific
+        length, this length is kept. If the levelname isn't part of the
+        Formatter, nothing changes.
+
+        After transforming the string inside the given record, the printing is
+        handled via the original StreamHandler."""
+
+        stream_handler = logging.StreamHandler()
+        stream_handler.setLevel(self.level)
+        stream_handler.setFormatter(self.formatter)
+
+        # check if the levelname is even part of the current Formatter
+        # if not, none of the transformations are necessary
+        fmt = self.formatter._fmt
+        if "levelname" in fmt:
+            levelname = ""
+            colors = {"DEBUG": "96",     # light cyan
+                      "INFO": "97",      # white
+                      "WARNING": "93",   # yellow
+                      "ERROR": "91",     # red
+                      "CRITICAL": "95"}  # magenta
+
+            # find the actual part of the Formatter that formats the levelname
+            fmt_placeholders = fmt.split(" ")
+            for placeholder in fmt_placeholders:
+                if "levelname" in placeholder:
+                    # set a local variable to what the Formatter would have
+                    # done. This is so that any changes to the string's length
+                    # (e.g. exactly 8 with "%(levelname)-8s") are preserved.
+                    levelname = placeholder % {"levelname": record.levelname}
+            # replace the record's levelname with the modified version.
+            record.levelname = "\033[{attr}m{lvlname}\033[0m".format(
+                attr=colors[record.levelname],
+                lvlname=levelname)
+        logging.StreamHandler.emit(stream_handler, record)
+
+
+def configure_logging():
+    """Configure Logging. Add a streamhandler and a custom AutoRemoteHandler"""
+
+    # create the logger
+    root = logging.getLogger()
+    root.setLevel(logging.DEBUG)
+
+    # create two formatter. They'll be used throughout the application.
+    nice_formatter = logging.Formatter(
+        "%(asctime)s %(levelname)-8s %(name)-12s %(message)s")
+    full_formatter = logging.Formatter(
+        "%(asctime)s %(levelname)-8s %(name)-12s %(message)s")
+    practical_formatter = logging.Formatter(
+        "%(asctime)s %(levelname)s %(name)s %(message)s",
+        time.strftime("%X"))
+
+    # create and add handlers
+    file_handler = logging.FileHandler("interface.log")
+    file_handler.setLevel(logging.DEBUG)
+    file_handler.setFormatter(full_formatter)
+    root.addHandler(file_handler)
+
+    console_handler = ColorStreamHandler()
+    console_handler.setLevel(logging.DEBUG)
+    console_handler.setFormatter(nice_formatter)
+    # turned off for now, since I don't want the logging in my console.
+    # root.addHandler(console_handler)
+
+    autoremote_handler = AutoRemoteHandler()
+    autoremote_handler.setLevel(logging.WARN)
+    autoremote_handler.setFormatter(practical_formatter)
+    root.addHandler(autoremote_handler)
+
+# import os.path
+
+# this_dir, this_filename = os.path.split(__file__)
+# print this_dir
+# print this_filename
+
+
+if __name__ == '__main__':
+    configure_logging()
+    logging.debug("-"*79)
+    logging.debug("Starting simple_cli")
+    logging.debug("-"*79)
     the_interface.start()
--- interface/simple_cli/__init__.py
+++ interface/simple_cli/__init__.py
@@ -2,7 +2,11 @@
 It attempts to connect to a server, by default on the current machine
 and then allows the user to enter commands"""
 import cmd
+import logging
 import requests
+
+# Create Logger
+logger = logging.getLogger(__name__)
 
 
 class CommandLineInterface(cmd.Cmd):
@@ -14,6 +18,7 @@ class CommandLineInterface(cmd.Cmd):
 
     def preloop(self):
         """Runs before the loop. Greets the user"""
+        logger.debug("Running the preloop-function, saying hi.")
         print("\n"
               "   ____    _    __  __    _    _   _ _____ _   _    _     ""\n"
               r"  / ___|  / \  |  \/  |  / \  | \ | |_   _| | | |  / \    ""\n"
@@ -26,6 +31,7 @@ class CommandLineInterface(cmd.Cmd):
 
     def postloop(self):
         """Runs after the loop."""
+        logger.debug("Running the postloop-function, saying bye.")
         print("\n"
               "   ____    _    __  __    _    _   _ _____ _   _    _     ""\n"
               r"  / ___|  / \  |  \/  |  / \  | \ | |_   _| | | |  / \    ""\n"
@@ -37,12 +43,15 @@ class CommandLineInterface(cmd.Cmd):
     def parseline(self, line):
         """Parses a line
         TODO: actually send the command to the server."""
+        logger.debug("Parsing '%s'.", line)
         ret = cmd.Cmd.parseline(self, line)
         try:
+            logger.info("Attempting to send '%s' to the server.", ret)
             requests.post("http://{url}:{port}/command"
                           .format(**self.connection),
                           {"key": ret[0], "params": ret[1], "comm": ret[2]})
         except requests.ConnectionError:
+            logger.error("Connection lost.")
             print "Connection lost."
             self.connection_error()
             return ("exit", "", "exit")
@@ -56,9 +65,11 @@ class CommandLineInterface(cmd.Cmd):
         pass
 
     def precmd(self, line):
+        logger.debug("Running the precmd-function")
         return cmd.Cmd.precmd(self, line)
 
     def postcmd(self, stop, line):
+        logger.debug("Running the postcmd-function")
         return cmd.Cmd.postcmd(self, stop, line)
 
     def do_EOF(self, line):
@@ -67,18 +78,24 @@ class CommandLineInterface(cmd.Cmd):
 
     def do_exit(self, line):
         """Exit the interface"""
+        logger.info("Received the command '%s'. Exiting", line)
         return True
 
     def connect(self):
         """attept connecting to an instance of Samantha"""
+        logger.info("Attempting to connect to 'http://%(url)s:%(port)s'",
+                    self.connection)
         print("Attempting to connect to 'http://{url}:{port}'"
               .format(**self.connection))
         try:
             requests.get("http://{url}:{port}/status"
                          .format(**self.connection))
             self.connection_attempts = 0
+            logger.info("Connection successful. "
+                        "Connection-attempts reset to 0.")
             print "Connection available."
         except requests.ConnectionError:
+            logger.error("Could not connect!")
             self.connection_error()
 
     def connection_error(self):
@@ -86,19 +103,28 @@ class CommandLineInterface(cmd.Cmd):
         failed attempts to the same server."""
         self.connection_attempts += 1
         if self.connection_attempts >= 3:
+            logger.fatal("Couldn't connect 3 times in a row. Exiting.")
             print "The connection failed 3 times in a row. Exiting."
             self.postloop()
             exit()
+        logger.info("Connection failed. Attempt(%d/3)",
+                    self.connection_attempts)
         print("The Connection to 'http://{url}:{port}/status' failed."
               .format(**self.connection))
+        logger.debug("Requiring userinput whether to try starting the server "
+                    "manually.")
         var = raw_input("Try to start the server remotely? (y/n) \n>>> ")
+        logger.debug("Userinput was '%s'.", var)
         while var not in ["y", "n"]:
             var = raw_input("Please use 'y' for yes or 'n' for no. \n>>> ")
         if var == "y":
-            # Start remotely
-            pass
+            logger.info("Attempting to start the Server remotely.")
+            # TODO Start the server remotely
         elif var == "n":
+            logger.debug("Requiring userinput whether to change the server's "
+                        "address.")
             var = raw_input("Enter a new host? (y/n) \n>>> ")
+            logger.debug("Userinput was '%s'.", var)
             while var not in ["y", "n"]:
                 var = raw_input(
                     "Please use 'y' for yes or 'n' for no. \n>>> ")
@@ -106,6 +132,9 @@ class CommandLineInterface(cmd.Cmd):
                 self.connection_attempts = 0
                 self.connection["url"] = raw_input(
                     "Please enter the new host: ")
+                logger.info("Address changed to '%s'. Connection-attempts "
+                            "reset to 0", self.connection["url"])
+        logger.info("Attempting to reconnect.")
         self.connect()
 
 
