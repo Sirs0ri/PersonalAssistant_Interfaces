b39b60ff4131dfe600165f2ed92e60be0cdf744d
Maximilian Ertl <ertl.maxi@gmail.com>
2016-05-06 18:56:30 +0200

    Switched to Autobahn's implementation of a websocket

--- interface/__main__.py
+++ interface/__main__.py
@@ -2,12 +2,12 @@
 from the root folder. After importing it, the interface will be
 started start"""
 
+from autobahn.twisted.websocket import WebSocketClientProtocol
 import logging
 import logging.handlers
-import os.path
-import time
-import simple_cli as the_interface
+
 import logger
+import basic_interface as interface
 
 try:
     import variables_private
@@ -23,9 +23,55 @@ except ImportError:
 LOGGER = logging.getLogger(__name__)
 
 
+class Interface(WebSocketClientProtocol):
+
+    def onConnect(self, response):
+        LOGGER.info("Server connected: %s", response.peer)
+        interface.on_connect(response)
+
+    def onOpen(self):
+        # self.sendMessage(u"Hello, world!".encode('utf8'))
+        # TODO Put some kind of authentication here
+        interface.on_open()
+
+        def sendInput():
+            val = interface.get_input()
+            if val == "exit":
+                interface.on_exit()
+                self.sendClose()
+            else:
+                self.sendMessage(val.encode('utf8'))
+                interface.message_sent(val)
+                self.factory.reactor.callLater(0.01, sendInput)
+
+        self.factory.reactor.callLater(0.01, sendInput)
+
+    def onMessage(self, payload, isBinary):
+        if isBinary:
+            LOGGER.info("Binary message received: %d", len(payload))
+        else:
+            LOGGER.info("Text message received: %s", payload.decode('utf8'))
+        interface.on_message(payload, isBinary)
+
+    def onClose(self, wasClean, code, reason):
+        if reason:
+            LOGGER.info(reason)
+        interface.on_close(wasClean, code, reason)
+        reactor.stop()
+
 
 if __name__ == '__main__':
+    # TODO: Establish conection separately.
     LOGGER.debug("-"*79)
-    LOGGER.debug("Starting simple_cli")
+    LOGGER.debug("Starting Interface")
     LOGGER.debug("-"*79)
-    the_interface.start()
+    interface.start()
+
+    from twisted.internet import reactor
+
+    from autobahn.twisted.websocket import WebSocketClientFactory
+    factory = WebSocketClientFactory()
+    factory.protocol = Interface
+
+    reactor.connectTCP("127.0.0.1", 9000, factory)
+    reactor.run()
---
+++ interface/basic_interface/__init__.py
@@ -0,0 +1,65 @@
+import logging
+
+# Create Logger
+LOGGER = logging.getLogger(__name__)
+
+
+def start():
+    LOGGER.debug("Running the preloop-function, saying hi.")
+    print("\n"
+          "   ____    _    __  __    _    _   _ _____ _   _    _     ""\n"
+          r"  / ___|  / \  |  \/  |  / \  | \ | |_   _| | | |  / \    ""\n"
+          r"  \___ \ / _ \ | |\/| | / _ \ |  \| | | | | |_| | / _ \   ""\n"
+          r"   ___) / ___ \| |  | |/ ___ \| |\  | | | |  _  |/ ___ \  ""\n"
+          r"  |____/_/   \_\_|  |_/_/   \_\_| \_| |_| |_| |_/_/   \_\ ""\n"
+          "                                                      hi~ ""\n"
+          "  Starting up!\n")
+
+
+def on_connect(response):
+    print "Connected to the Server at {}\n".format(response.peer)
+    print "Please enter your command:"
+    return
+
+
+def on_open():
+    return
+
+
+def message_sent(message):
+    print "Message '{}' sent successfully.".format(message)
+
+
+def on_message(payload, isBinary):
+    if isBinary:
+        print "Binary message received: {}".format(len(payload))
+    else:
+        print "Text message received: {}".format(payload.decode('utf8'))
+    return
+
+
+def on_exit():
+    print "Received the command to close the connection."
+
+
+def on_close(wasClean, code, reason):
+    LOGGER.debug("Running the postloop-function, saying bye.")
+    print("\n"
+          "   ____    _    __  __    _    _   _ _____ _   _    _     ""\n"
+          r"  / ___|  / \  |  \/  |  / \  | \ | |_   _| | | |  / \    ""\n"
+          r"  \___ \ / _ \ | |\/| | / _ \ |  \| | | | | |_| | / _ \   ""\n"
+          r"   ___) / ___ \| |  | |/ ___ \| |\  | | | |  _  |/ ___ \  ""\n"
+          r"  |____/_/   \_\_|  |_/_/   \_\_| \_| |_| |_| |_/_/   \_\ ""\n"
+          "                                                     bye~ ")
+
+
+def get_input():
+    return raw_input(">>> ").decode('utf8')
+
+
+if __name__ == "__main__":
+    print ("This application is not meant to be executed on its own. To start "
+           "it, please run '__main__.py' in it's parent directory, or the cli-"
+           "command 'python interface' in the directory above (that should be "
+           "'/PersonalAssistant_Interfaces').\n")
+    var = raw_input("Press the enter-key to exit.\n")
